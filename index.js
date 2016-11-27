09268d5b0b1b98f7ed31c20139e41fed94dfc16e
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-11-27 10:43:58 -0300

    Add basic map funcitonality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* jshint esversion:6 */
 
 import message from './lib/message.js';
+import map from './lib/map.js';
+import basicStyles from './lib/styles/basicStyles.css';
 
 console.log(message());
