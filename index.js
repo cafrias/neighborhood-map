7517e90a420d7bbf7cb2ac48483279f2f3aaac0f
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-11-28 17:22:39 -0300

    Add basic UI Interaction: Toggle Menu

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 
 import map from './lib/map.js';
 import basicStyles from './lib/styles/basicStyles.css';
+import SideBar from './lib/sidebar.js';
+
+function main() {
+	var sideBar = new SideBar();
+}
+
+document.addEventListener('DOMContentLoaded', main);
