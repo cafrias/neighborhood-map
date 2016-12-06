db7f719e36ce71b3ab2cd634ca035f4f7c35ecf5
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-12-06 19:47:14 -0300

    Add redesign

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /* jshint esversion:6 */
 
-import map from './lib/map/map.js';
+// Import basic styles
+import normalizeCss from 'normalize-css/normalize.css';
 import basicStyles from './lib/basicStyles.css';
+
+// Import JS dependencies
+import map from './lib/map/map.js';
 import SideBar from './lib/sidebar/sidebar.js';
 
 function main() {
