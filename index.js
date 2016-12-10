6a88a6b9ebb5ed48de310f9650145886b5be397d
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-12-10 11:06:39 -0300

    Add basic ko functionality

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import normalizeCss from 'normalize-css/normalize.css';
 import basicStyles from './lib/basicStyles.css';
 
 // Import JS dependencies
-import map from './lib/map/map.js';
+import Map from './lib/map/map.js';
 import SideBar from './lib/sidebar/sidebar.js';
+import ViewModel from './lib/viewModel/viewModel.js';
+import locations from './data/locations.js';
+import ko from 'knockout';
 
-function main() {
+global.main = () => {
+	global.map = new Map(locations);
+	var viewModel = new ViewModel(ko, map.markers);
 	var sideBar = new SideBar();
-}
 
-document.addEventListener('DOMContentLoaded', main);
+	ko.applyBindings(viewModel);
+	viewModel.displayLocations(map);
+};
