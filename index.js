7ba8b05cf3b89ac2789f48afee1570e3a3020252
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-12-12 21:12:15 -0300

    Add basic click filter functionality

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ import normalizeCss from 'normalize-css/normalize.css';
 import basicStyles from './lib/basicStyles.css';
 
 // Import JS dependencies
-import Map from './lib/map/map.js';
+
 import SideBar from './lib/sidebar/sidebar.js';
 import ViewModel from './lib/viewModel/viewModel.js';
 import locations from './data/locations.js';
 import ko from 'knockout';
 
 global.main = () => {
-	global.map = new Map(locations);
-	var viewModel = new ViewModel(ko, map.markers);
+	var viewModel = new ViewModel(ko, locations);
 	var sideBar = new SideBar();
 
-	ko.applyBindings(viewModel);
-	viewModel.displayLocations(map);
+	ko.applyBindings(viewModel, document.getElementById('sidebar'));
+	viewModel.displayOnMap();
 };
