b41bd5e058a9b154c4ffea382cd92296a747b752
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2016-12-19 12:28:56 -0300

    Add places information to infoWindow

--- data/locations.js
+++ data/locations.js
@@ -12,47 +12,53 @@ https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDlp-FPcPh
 var locations = [
 	{
 		title: 'Sanatorio Fueguino',
-		position: {lat: -53.7707509, lng: -67.70989999999999}
+		position: {lat: -53.7707509, lng: -67.70989999999999},
+		placeId: 'ChIJBUUZFDkWS7wRDV3R1jDDw8o'
 	},
 	{
 		title: 'Nuevo Centro',
-		position: {lat: -53.772523, lng: -67.7107274}
-	},
-	{
-		title: 'GECALCTDF',
-		position: {lat: -53.7722445, lng: -67.7097618}
+		position: {lat: -53.772523, lng: -67.7107274},
+		placeId: 'ChIJJZmjMD8WS7wRdUNDumRAGPU'
 	},
 	{
 		title: 'Soki Studio',
-		position: {lat: -53.77077949999999, lng: -67.7116635}
+		position: {lat: -53.77077949999999, lng: -67.7116635},
+		placeId: 'ChIJ09AozD4WS7wRy1URU9IjVlc'
 	},
 	{
 		title: 'Solo Música',
-		position: {lat: -53.7714647, lng: -67.7128068}
+		position: {lat: -53.7714647, lng: -67.7128068},
+		placeId: 'ChIJlQkTqUQWS7wRBhy__wpNnN4'
 	},
 	{
 		title: 'Veterinaria Leiva',
-		position: {lat: -53.77277239999999, lng: -67.7106228}
+		position: {lat: -53.77277239999999, lng: -67.7106228},
+		placeId: 'ChIJzdEtOD8WS7wRBtWjKdZyJRE'
 	},
 	{
 		title: 'Heladeria Almendra',
-		position: {lat: -53.76965, lng: -67.716453}
+		position: {lat: -53.76965, lng: -67.716453},
+		placeId: 'ChIJDd_k2zgWS7wRaFGb6cUEBps'
 	},
 	{
 		title: 'Stylos Coiffeur',
-		position: {lat: -53.7696769, lng: -67.71630669999999}
+		position: {lat: -53.7696769, lng: -67.71630669999999},
+		placeId: 'ChIJ0UVqKjwWS7wR3jqmgWJl6tI'
 	},
 	{
 		title: 'Farmacia del Pueblo II',
-		position: {lat: -53.769576, lng: -67.71649649999999}
+		position: {lat: -53.769576, lng: -67.71649649999999},
+		placeId: 'ChIJaWKoKzwWS7wRrMZsR3Yg8dc'
 	},
 	{
 		title: 'Multirubro Las Vegas',
-		position: {lat: -53.7695717, lng: -67.7166314}
+		position: {lat: -53.7695717, lng: -67.7166314},
+		placeId: 'ChIJjw0UKzwWS7wRDca2SIW_k_k'
 	},
 	{
 		title: 'Ksanuova Pizza Bar',
-		position: {lat: -53.76946710000001, lng: -67.7165602}
+		position: {lat: -53.76946710000001, lng: -67.7165602},
+		placeId: 'ChIJfeuGKzwWS7wRU6a1uZdvPRA'
 	}
 ];
 
